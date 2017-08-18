2f455b6669be8474539ba0affbd6a5baa0a38f76
natarajnattu <atchula.nataraj@live.in>
2017-08-18 18:06:38 +0530

    removing node modules

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -1,8 +1,6 @@
 'use strict';
-
-
 var mongoose = require('mongoose'),
-  Task = mongoose.model('Tasks');
+Task = mongoose.model('Tasks');
 
 exports.list_all_tasks = function(req, res) {
   Task.find({}, function(err, task) {
@@ -11,9 +9,6 @@ exports.list_all_tasks = function(req, res) {
     res.json(task);
   });
 };
-
-
-
 
 exports.create_a_task = function(req, res) {
   var new_task = new Task(req.body);
@@ -24,7 +19,6 @@ exports.create_a_task = function(req, res) {
   });
 };
 
-
 exports.read_a_task = function(req, res) {
   Task.findById(req.params.taskId, function(err, task) {
     if (err)
@@ -32,7 +26,6 @@ exports.read_a_task = function(req, res) {
     res.json(task);
   });
 };
-
 
 exports.update_a_task = function(req, res) {
   Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
@@ -42,10 +35,7 @@ exports.update_a_task = function(req, res) {
   });
 };
 
-
 exports.delete_a_task = function(req, res) {
-
-
   Task.remove({
     _id: req.params.taskId
   }, function(err, task) {
